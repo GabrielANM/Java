5b2487e68559688d5ee66cb0c6c0a0bafa85c248
mauriciocoruja <mbjr@outlook.com.br>
2019-06-12 17:29:01 -0300

    ultimo save do dia

--- Quarta_feira_12_06.sql
+++ Quarta_feira_12_06.sql
@@ -10,8 +10,41 @@ use generation;
 /* criando uma tabela */
 create table alunos (
 	id int(11) primary key auto_increment not null,
-    nome varchar(30),
-    sobrenome varchar(30),
-    email varchar(30),
-    idade int(3),
+    nome varchar(50),
+    sobrenome varchar(50),
+    email varchar(50),
+    data_nascimento date,
+    cpf varchar(11) unique
 );
+
+create table professores (
+	id int(11) primary key auto_increment not null,
+    nome varchar(50),
+    sobrenome varchar(50),
+    email varchar(50),
+    data_nascimento date,
+    cpf varchar(11) unique
+);
+
+create table cursos (
+	id int(11) primary key auto_increment not null,
+    nome varchar(50)
+);
+
+create table turmas (
+	id int(11) primary key auto_increment not null,
+    descricao varchar(50),
+    periodo varchar(20),
+    id_alunos int(11),
+    id_professores int(11),
+    id_cursos int(11)
+);
+
+/* exibindo todas as tabelas do meu banco de dados */
+show tables;
+
+/* descrever os detalhes de uma tabela */
+describe alunos;
+
+
+
