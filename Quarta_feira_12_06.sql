1a70704de2cfe804b4d8064f9f052341094d7929
unknown <Digital@DH-A2-25.dhbrasil.digitalhouse.com>
2019-06-13 16:12:54 -0300

    salvando o mysql

--- Quarta_feira_12_06.sql
+++ Quarta_feira_12_06.sql
@@ -17,6 +17,36 @@ create table alunos (
     cpf varchar(11) unique
 );
 
+INSERT INTO alunos (
+	nome, 
+	sobrenome, 
+	email, 
+	data_nascimento, 
+	cpf
+) VALUES (
+	"João", 
+	"Camargo", 
+	"joão@digitalhouse.com", 
+	"2001-05-22", 
+	"12345678900"
+);
+
+SELECT id, nome, sobrenome, email, data_nascimento, cpf
+FROM alunos;
+
+SELECT * FROM alunos;
+
+SELECT nome, sobrenome FROM alunos;
+
+SELECT * FROM alunos WHERE nome = "João";
+SELECT * FROM alunos WHERE id = 2 OR cpf = "47077779882";
+
+DELETE FROM alunos WHERE id = 1;
+
+UPDATE alunos SET sobrenome = "Da Galera" WHERE id = 2;
+
+SELECT sobrenome FROM alunos WHERE id = 2;
+
 create table professores (
 	id int(11) primary key auto_increment not null,
     nome varchar(50),
@@ -26,9 +56,29 @@ create table professores (
     cpf varchar(11) unique
 );
 
+INSERT INTO professores (
+	nome,
+    sobrenome,
+    email,
+    data_nascimento,
+    cpf
+) VALUES (
+	"Jorge",
+    "Ferreira",
+    "jorge@digitalhouse.com",
+    "1980-08-30",
+    12345678900
+);
+
 create table cursos (
 	id int(11) primary key auto_increment not null,
     nome varchar(50)
+);
+
+INSERT INTO cursos (
+    nome
+) VALUE (
+	"Generation"
 );
 
 create table turmas (
@@ -40,11 +90,42 @@ create table turmas (
     id_cursos int(11)
 );
 
+INSERT INTO turmas (
+	descricao,
+    periodo,
+    id_alunos,
+    id_professores,
+    id_cursos
+) VALUE (
+	"É UMA TURMA FODA!!",
+    "INTEGRAL",
+    1,
+    1,
+    1
+);
+
+-- DROP TABLE turmas;
+
+SELECT * FROM turmas;
+
 /* exibindo todas as tabelas do meu banco de dados */
 show tables;
 
 /* descrever os detalhes de uma tabela */
 describe alunos;
 
+-- DROP DATABASE generation;
+SHOW TABLES;
 
+ALTER TABLE turmas ADD FOREIGN KEY (id_alunos) REFERENCES alunos (id);
+ALTER TABLE turmas ADD FOREIGN KEY (id_professores) REFERENCES professores (id);
+ALTER TABLE turmas ADD FOREIGN KEY (id_cursos) REFERENCES cursos (id);
 
+DESCRIBE turmas;
+
+ALTER TABLE turmas DROP FOREIGN KEY turmas_ibfk_2;
+
+ALTER TABLE alunos RENAME TO aluno;
+ALTER TABLE cursos RENAME TO curso;
+ALTER TABLE professores RENAME TO professor;
+ALTER TABLE turmas RENAME TO turma;
