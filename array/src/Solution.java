7299f12d539ac45bb2b89bde9a37ce5a709f6b94
GabrielANM <bilbor8@gmail.com>
2019-07-01 09:38:23 -0300

    adicionando o desafio do hackerrank

--- array/src/Solution.java
+++ array/src/Solution.java
@@ -2,31 +2,23 @@
 public class Solution {
 
     public Solution() {
+
     }
 
     public static boolean canWin(int leap, int[] game) {
         // Return true if you can win the game; otherwise, return false.
-        int posAtual = 0, posFinal = game.length -1, j = 0;
+        int posAtual = 0, posFinal = game.length - 1;
+
         for (int i = 0; i < game.length; i++) {
-            if (i < game.length && i + leap <= game.length + 1) {
-                if (game[i + 1] == 0) {
-                    posAtual = i + 1;
-                } else if (game[i + 1] >= game.length) {
-                    posAtual = posFinal;
-                    break;
-                } else if (posAtual != posFinal) {
-                    while (game[i + j] < game.length) {
-                        if (game[i + j] == 0 && game[i + j] < game.length) {
-                            posAtual = i + leap;
-                        } else if (game[i + j] >= game.length) {
-                            posAtual = posFinal;
-                            break;
-                        }else if (j <= leap) {
-                            j++;
+            if (game[i] == 0) {
+                posAtual = i;
+            } else {
+                for (int j = 1; i + j <= game.length - 1; j++) {
+                    if (j < leap ) {
+                        if (game[i + j] == 0 || i + j == posFinal) {
+                            posAtual = i + j;
                         }
                     }
-                } else {
-                    break;
                 }
             }
         }
